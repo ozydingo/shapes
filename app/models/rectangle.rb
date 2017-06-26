d906587cbea41dbf8958c914a200d65c5d412bde
Andrew Schwartz <ozydingo@gmail.com>
2017-06-26 11:53:08 -0400

    having bigly trouble

--- app/models/rectangle.rb
+++ app/models/rectangle.rb
@@ -2,4 +2,17 @@ class Rectangle < ActiveRecord::Base
   def area
     width * height
   end
+
+  def biggest(smaller_than = nil)
+    biggest = 0
+    Rectangle.all.each do |rect|
+      this_area = rect.area
+      if this_area > biggest
+        biggest = this_area
+      end
+    end
+
+    biggest = Rectangle.where(area: biggest).first
+    return biggest.area
+  end
 end
